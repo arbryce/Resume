b40d0bae8437d81268eff35e30ff3cdb7a7cb0ce
arbryce <arbryce@yahoo.com>
2023-02-27 20:22:11 -0600

    301 Project: Add bulk users to AD

---
+++ BulkUsers.ps1
@@ -0,0 +1,47 @@
+# Import AD module for cmdlets
+Import-Module activedirectory
+
+# Set variable
+$Users = Import-csv <filepath>
+
+# CSV file variables and loop through details
+foreach ($User in $Users) {
+$LastName = $User.lastname
+$FirstName = $User.firstname
+$Title = $User.jobtitle
+$Department = $User.department
+$OU = $User.ou
+$UserName = $User.logonname
+$Email = $User.email
+$Phone = $User.telephonenumber
+$Street = $User.streetaddress
+$City = $User.city
+$State = $User.state
+$Zip = $User.zip
+
+# If user already exists
+if (Get-ADUser -F {SamAccountName -eq $UserName})
+    {
+        Write-Warning "A user with this username $UserName already exists." }
+
+# If user does not exist
+        else {
+            New-ADUser '
+            -FirstName $FirstName
+            -LastName $LastName
+            -DisplayName "$FirstName $LastName" 
+            -JobTitle $Title
+            -Department $Department
+            -Path $OU
+            -SamAccountName $UserName 
+            -UserPrincipalName "$UserName@corp.globexpower.com"
+            -Email $Email
+            -Telephone $Phone
+            -Street $Street
+            -City $City
+            -State $State
+            -Zip $Zip
+            -ChangePasswordAtLogon $True '
+
+        }
+    }
