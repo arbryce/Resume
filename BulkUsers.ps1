b0128877c96a4bc11748db491112e1d278a481e3
arbryce <arbryce@yahoo.com>
2023-02-27 21:33:04 -0600

    update

--- BulkUsers.ps1
+++ BulkUsers.ps1
@@ -7,18 +7,18 @@ $Users = Import-csv <filepath>
 # CSV file variables and loop through details
 foreach ($User in $Users) 
 {
-$LastName = $User.lastname
-$FirstName = $User.firstname
-$Title = $User.jobtitle
-$Department = $User.department
-$OU = $User.ou
-$UserName = $User.username
-$Email = $User.email
-$Phone = $User.telephonenumber
-$Street = $User.streetaddress
-$City = $User.city
-$State = $User.state
-$Zip = $User.zip
+    $First = $User.firstname
+    $Last = $User.lastname
+    $Title = $User.jobtitle
+    $Department = $User.department
+    $OU = $User.ou
+    $UserName = $User.username
+    $Email = $User.email
+    $Phone = $User.telephonenumber
+    $Street = $User.streetaddress
+    $City = $User.city
+    $State = $User.state
+    $Zip = $User.zip
 
 # If user already exists
 if (Get-ADUser -F {SamAccountName -eq $UserName})
@@ -29,21 +29,21 @@ if (Get-ADUser -F {SamAccountName -eq $UserName})
         else {
             New-ADUser 
             $details = @{
-            FirstName = $FirstName
-            LastName = $LastName
-            DisplayName = "$FirstName $LastName" 
-            JobTitle = $Title
-            Department = $Department
-            Path = $OU
-            SamAccountName = $UserName 
-            UserPrincipalName = "$UserName@corp.globexpower.com"
-            Email = $Email
-            Telephone = $Phone
-            Street = $Street
-            City = $City
-            State = $State
-            Zip = $Zip
-            ChangePasswordAtLogon = $True 
+                SamAccountName = $UserName 
+                FirstName = $First
+                LastName = $Last
+                DisplayName = "$First $Last" 
+                JobTitle = $Title
+                Department = $Department
+                Path = $OU
+                UserPrincipalName = "$UserName@corp.globexpower.com"
+                Email = $Email
+                Telephone = $Phone
+                Street = $Street
+                City = $City
+                State = $State
+                Zip = $Zip
+                ChangePasswordAtLogon = $True 
             } #end details
         }
 }
