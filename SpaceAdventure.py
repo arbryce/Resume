237e2a3d08d965c6ec65f5a42cb80cf3b509f751
arbryce <arbryce@yahoo.com>
2023-03-06 14:49:04 -0600

    WIP update

--- SpaceAdventure.py
+++ SpaceAdventure.py
@@ -1,5 +1,6 @@
 # Setup
 import random
+from random import randint
 
 yes_no = ["yes", "no"]
 directions = ["left", "right", "forward", "backward"]
@@ -25,3 +26,36 @@ while response not in yes_no:
         print("Does not compute. Let's try that again, shall we?\n")
 
 # Part 1: Fitness Test 
+print("We first need to ensure you're mentally adept for such a strenuous undertaking.")
+
+def generator():
+    return randint(1,5)
+def rand_guess():
+    random_number = generator()
+    guess_left = 3
+    flag = 0
+
+    while guess_left > 0:
+        guess = int(input("You have 3 chances to correctly guess a number from 1 to 5\n"))
+        
+        if guess == random_number:
+            flag = 1
+            break
+
+        else:
+            print("Ooh, so close. Try again!")
+        guess_left -= 1
+
+    if flag == 1:
+        return True
+ 
+    else:
+        return False
+ 
+if __name__ == '__main__':
+    if rand_guess() is True:
+        print("Congratulations! You are ready to explore the depths of Uranus!")
+    else :
+        print("Sorry, looks like you didn't make the cut.")
+
+# Part 2: 
